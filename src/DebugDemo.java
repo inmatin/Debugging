a816460d1d19da05486158de8bb45de8eaf5f751
Ishtiaque Matin <nurul_matin@hotmail.com>
2022-03-11 22:47:38 -0500

    Add files via upload

---
+++ src/DebugDemo.java
@@ -0,0 +1,19 @@
+
+public class DebugDemo {
+
+	public static void main(String[] args) {
+
+		//int aaa = 5/0;
+		
+		int number1 = 5;
+		int number2 = 10;
+		int sum = number1 + number2;
+
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(sum);
+
+
+	}
+
+}
